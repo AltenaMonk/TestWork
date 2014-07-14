b8cc961f4e2045fec6fdd479db713599c590ddd7
testr00t <mapr00tmap@gmail.com>
2014-07-14 17:24:27 +0000

    ввод/вывод строки

--- Task_0000_String_to_words/main.cpp
+++ Task_0000_String_to_words/main.cpp
@@ -1,1 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    std::cout << "Enter your line:";
+    char text[80] = {0, };
+    gets(text);
+    std::cout << "line out:" << text << std::endl;
+    return 0;
+}
