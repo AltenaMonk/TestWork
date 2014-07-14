827417c95c77ac2960ec6d9ff29c4c439d787dae
testr00t <mapr00tmap@gmail.com>
2014-07-14 20:25:05 +0000

    words added

--- Task_0000_String_to_words/main.cpp
+++ Task_0000_String_to_words/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <cstdio>
+#define LEN 80 //длина строки
 
 int main()
 {
-    std::cout << "Enter your line:";
-    char text[80] = {0, };
+    std::cout << "Your line:";
+    char text[LEN] = {0, };
     gets(text);
     std::cout << "line out:" << text << std::endl;
+    int space_index = 0;
+    for (int i = 0; i <= LEN; ++i)
+    {
+        if (text[i] == ' ')
+        {
+            for (int k = space_index; k <= i; ++k)
+            {
+                std::cout << text[k];
+                space_index = k;
+            }
+             std::cout <<""<< std::endl;
+        }
+    }
     return 0;
 }
