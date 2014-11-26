f67aaf32fdc91e3c9d9def95d211c608dfa5abc8
Radyuk Sergey <s.radyuk@mpsquickpay.com>
2014-11-26 13:36:46 +0600

    Поправил явные ошибки, прокоментировал что и почему поправил.

--- Task_0006_functions/main.cpp
+++ Task_0006_functions/main.cpp
@@ -2,51 +2,88 @@
 #include <memory.h>
 #include <cstdio>
 
-/// Общее стилистическое замечание, лучше именовать функции по методу объект_действие, примеры:
-/// string_lenght
-/// string_copy
-/// string_print  а не print_string
-/// string_resize а не resize_string
-/// Это добавляет читабельности и лёгкости запоминания, а то каждый раз думать что вперёд идёт объект или действие.
-/// Ну и такой порядок записи станет более привычен когда будем классы проходить.
-
-/// По сути всё правильно. Для этой функции только маленькие замечания.
-int string_lenght (char * string)
+int string_lenght(char * string)
 {
+    /// Защита от дурака.
+    if (string == NULL)
+    {
+        return 0;
+    }
     int i = 0;
-    /// Писать i = 0 в данном месте не обязательно, можно просто for (; string[i] != 0; ++i)
-    /// Все 3 поля являются не обязательными.
     for (; string[i] != 0; ++i)
     {
-        /// Это тоже не надо, спокойно можно писать.
-        /// for (; string[i] != 0; ++i)
-        /// {
-        /// }
     }
-    return i + 1;
+    /// Возвращать правильнее размер занимаемый полезными данными, а не техническим нулём. return i; a не return i + 1;
+    return i;
 }
-/// Тут приведены мои варианты написания аналогичных функций с теми же знаниями, что есть у тебя:
-//int string_lenght(char * string)
-//{
-//    int i = 0;
-//    for (i = 0; string[i] != 0; ++i)
-//    {
-//    }
-//    return i + 1;
-//}
 
-/// По сути всё правильно. Для этой функции только маленькие замечания.
+int string_print(char * string)
+{
+    /// Защита от дурака.
+    if (string == NULL)
+    {
+        return 0;
+    }
+    int i = 0;
+    for (; string[i] != 0; ++i)
+    {
+        std::cout << string[i];
+    }
+    /// Необходимо вернуть количество напечатанных символов. Иначе зачем вообще стоит int в описании функции?
+    return i;
+}
+
+int string_copy(char * from, char * to)
+{
+    /// Защита от дурака.
+    if (from == NULL || to == NULL)
+    {
+        return 0;
+    }
+    int i = 0;
+    /// Здесь правильнее копировать всё до нулевого символа from[i] != 0, а не до нулевой строкм from != 0, так как иначе это бесконечный цикл.
+    for (; from[i] != 0; ++i)
+    {
+        to[i] = from[i];
+    }
+    return i;
+}
+
+/// @brief Выделяет память под строку в указанном количестве. Использует в качестве вспомагательной string_copy.
+/// @param string Исходная строка.
+/// @param new_size Новый размер буфера.
+void string_resize (char ** string, int size_new)
+{
+    /// Если указатель на изменяемую строку равен 0, то выходим ни чего не сделав.
+    if (string == NULL)
+    {
+        return;
+    }
+
+    /// Выделяем память в указанном количестве под новую строку.
+    char * newString = new char[size_new + 1];
+    memset(newString, 0, size_new + 1);
+
+    /// Копируем старую строку в новую.
+    string_copy(*string, newString);
+
+    /// Если под изменяемую строку была выделена память, то освободим эту память.
+    if (*string != NULL)
+    {
+        delete [] *string;
+    }
+
+    /// Начнём хранить по указателю на строку нашу вовую строку.
+    *string = newString;
+}
+
 int word_counter (char * string)
 {
     int count = 1;
-    /// Лучше вычислять длинну 1 раз и потом использовать постоянно.
-    /// Примерно так:
-    /// int size = string_lenght(string);
-    /// for (int i = 0; i < size; ++i)
     int size = string_lenght(string);
+    /// Если возвращать размер полезных данных, то правильнее будет i <= size;
     for (int i = 0; i < size; ++i)
     {
-        /// Зачем проверка на 0ой символ, у тебя длинна и так уже проверяет до 0ого символа.
         if (string[i] == ' ')
         {
             ++count;
@@ -54,127 +91,7 @@ int word_counter (char * string)
     }
     return count;
 }
-/// Тут приведены мои варианты написания аналогичных функций с теми же знаниями, что есть у тебя:
-//int word_count(char * string)
-//{
-//    int count = 1;
-//    int size = string_lenght(string);
-//    for (int i = 0; i < size; ++i)
-//    {
-//        if (string[i] == ' ')
-//        {
-//            ++count;
-//        }
-//    }
-//    return count;
-//}
-//int word_count(char * string)
-//{
-//    int count = 1;
-//    for (int i = 0; string[0] != 0; ++i)
-//    {
-//        if (string[i] == ' ')
-//        {
-//            ++count;
-//        }
-//    }
-//    return count;
-//}
 
-/// По сути всё правильно. Для этой функции только малеькие замечания.
-int string_print (char * string)
-{
-    /// Лучше вычислять длинну 1 раз и потом использовать постоянно.
-    /// Примерно так:
-    /// int size = string_lenght(string);
-    /// for (int i = 0; i < size; ++i)
-    int size = string_lenght(string);
-    for (int i = 0; i < size; ++i)
-    {
-        /// Зачем это, если длинна и так считается до 0 не включительно? Думаю можно условие полностью вырезать, и оставить только std::cout << string[i];
-        {
-            std::cout << string[i];
-        }
-    }
-}
-/// Тут приведены мои варианты написания аналогичных функций с теми же знаниями, что есть у тебя:
-//int string_print(char * string)
-//{
-//    int size = string_lenght(string);
-//    for (int i = 0; i < size; ++i)
-//    {
-//        std::cout << string[i];
-//    }
-//}
-//int string_print(char * string)
-//{
-//    for (int i = 0; string[i] != 0; ++i)
-//    {
-//        std::cout << string[i];
-//    }
-//}
-
-
-/// Ошибка тут есть.
-/// char * new_string передавать нельзя.
-/// Пример, ты извне передал сюда адрес 0x2000 => Переменная new_string = 0x2000, это просто число.
-/*int resize_string (char * origin_string, char * new_string)
-{
-    for (int i = 0; string_lenght(new_string) < string_lenght(origin_string); ++i)
-    {
-        /// Здесь ты выделил память и тебе вернули адрес 0x3000, тоже просто число, но по этому адресу для тебя выделили требуемую тобой память.
-        char * temp = new char[string_lenght(new_string) * 2];
-        /// Затем ты здесь удалил память по адресу 0x2000.
-        delete [] new_string;
-        /// Здесь переменная new_string стала равна 0x3000, просто новому числу.
-        /// НО!!! Эта переменная после выхода из функции ни где не останется. Эта информация будет потеряна.
-        new_string = temp;
-        /// Надо либо в качестве результата возвращать new_string, а в месте приёмки присваивать переменной новый указатель.
-        /// Либо принимать указатель на указатель: char ** new_string и менять значение *new_string = temp.
-    }
-    return 0;
-}*/
-/// А вообще эту функцию лучше реализовывать как-то так:
-/// @brief Выделяет память под строку в указанном количестве. Использует в качестве вспомагательной string_copy.
-/// @param string Исходная строка.
-/// @param new_size Новый размер буфера.
-/// void string_resize(char ** string, int new_size);
-
-void string_resize (char ** string, int size_new)
-{
-    char ** temp = new char[10];
-    memset(temp, 0, 10 * sizeof(char *));
-    for (int i = 0; i < 10; ++i)
-    {
-        temp[i] = new char[size_new];
-        memset(temp[i], 0, size_new * sizeof(char));
-    }
-
-    int * old_size = new int[100];
-    memset(old_size, 0, 100 * sizeof(int));
-    for (int i = 0; i < 10; ++i)
-    {
-        old_size[i] = string_copy(string[i],temp[i]);
-    }
-    for (int i = 0; i < 10; ++i)
-    {
-        for (int k = 0; )
-    }
-}
-
-
-/// Тут есть идейные ошибки.
-int string_copy (char * origin_string, char * copied_string)
-{
-    /// Тут идеалогически не хорошо выделять память, это должен сделать тот кто вызывает функцию.
-    /// Ты только копируешь.
-    /// Может там раньше был ОГРОМНЫЙ буфер(1024*1024*1024), а ты его заменил на что-то маленькое(10*2).
-    for (int i = 0; origin_string != 0; ++i)
-    {
-        copied_string[i] = origin_string[i];
-    }
-    return i;
-}
 /// А вообще эту функцию лучше реализовывать как-то так:
 /// @brief Копирует одну строку в другую, не меняет буферы, так что вызывающая сторона должна обеспечить достаточность памяти.
 /// @param from Исходная строка.
@@ -204,11 +121,6 @@ int string_copy (char * origin_string, char * copied_string)
     }
     return 0;
 }
-/*
-int print_string (char * string)
-{
-
-}
 */
 
 /// Примерно, с моей точки зрения, должно использование этого выглядеть так:
@@ -225,22 +137,26 @@ int main()
 {
     char * text = new char[1024];
     memset(text, 0, 1024 * sizeof(char));
+    std::cout << "Введите строку для кирпичинизции:" << std::endl;
     gets(text);
-    char * test_string = new char[10];
-    /// Ой-ой-ой, а где там у нас будет первый ноль при определении длинны? Тут надо писать полный размер, его знаешь только ты. memset(test_string, 0, 10 * sizeof(test_string[0]));
-    memset(test_string, 0, string_lenght(test_string));
-    std::cout << "Длина строки (исходная, тестовая)" << string_lenght(text) << ", " << string_lenght(test_string) << std::endl;
-    std::cout << "Количество слов:" << word_counter(text) << std::endl;
-    std::cout << "Исходная строка:" << std::endl;
-    std::cout << print_string(text) << std::endl;
-    string_copy(text,test_string);
-    std::cout << print_string(test_string) << std::endl;
+    /// Выделять память сдесь нет ни какого смысла, так как в последствии мы её все равно освобождаем.
+    char * test_string = NULL;
+    std::cout << "Длина строки (исходная, тестовая): " << string_lenght(text) << ", " << string_lenght(test_string) << std::endl;
+    std::cout << "Количество слов: " << word_counter(text) << std::endl;
+    std::cout << "Исходная строка: " << std::endl;
+    string_print(text);
+    std::cout << std::endl;
+    /// Здесь мы заботимся о том, что бы для нашёй строки хватило места.
+    string_resize(&test_string, string_lenght(text));
+    string_copy(text, test_string);
+    std::cout << "Скопированная строка:" << std::endl;
+    string_print(test_string);
+    std::cout << std::endl;
 
-    /// А кто буфер освобождать будет?
-    /// char * test_string = new char[10];
-
-    /// А кто буфер освобождать будет?
-    /// char * text = new char[1024];
+    /// А кто буфер освобождать будет?!!!!!!!!!!!!!!
+    /// А кто буфер освобождать будет?!!!!!!!!!!!!!!
+    delete [] test_string;
+    delete [] text;
 
     return 0;
 }
