8be06c879adbf75f8a8b687e4e8f390cdb422c09
testr00t <mapr00tmap@gmail.com>
2015-06-18 15:49:08 +0600

    in devel 18.06.2015

--- Task_0007_money_storage/money_storage.cpp
+++ Task_0007_money_storage/money_storage.cpp
@@ -1,7 +1,45 @@
-
+#include <cstddef>
 struct money_value
 {
-    int volume_int;
-    int volume_dec;
-    int volume_hund;
+    int value_int;
+    int value_dec;
+    int value_hund;
 };
+
+void add (money_value * first, money_value * second)
+{
+    if (first == NULL || second == NULL)
+    {
+        return;
+    }
+    money_value.value_int = first->value_int + second->value_int;
+    money_value.value_dec = first->value_dec + second->value_dec;
+    money_value.value_hund = first->value_hund + second->value_hund;
+}
+
+void substruct (money_value * first, money_value * second)
+{
+    if (first == NULL || second == NULL)
+    {
+        return;
+    }
+    first->value_int = first->value_int - second->value_int;
+    first->value_dec = first->value_dec - second->value_dec;
+    first->value_hund = first->value_hund - second->value_hund;
+}
+
+void multiply (money_value * first, int * second)
+{
+    if (first == NULL || second == NULL)
+    {
+        return;
+    }
+    first->value_int = first->value_int * second;
+    first->value_dec = first->value_dec * second;
+    first->value_hund = first->value_hund * second;
+}
+
+/*void print (money_value * value)
+{
+
+}*/
