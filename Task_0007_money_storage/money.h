8fc3aa3587150837f9e322786b7676241753b91c
testr00t <mapr00tmap@gmail.com>
2015-06-30 15:15:11 +0600

    modul 30.06.2015

---
+++ Task_0007_money_storage/money.h
@@ -0,0 +1,63 @@
+#include <iostream>
+struct money_value
+{
+private:
+
+    int value;
+
+public:
+    money_value (int _int, int _hund)
+    {
+        value = _int * 100 + _hund;
+    }
+
+    void print ()
+    {
+        if (value >= 10)
+        {
+            std::cout << value / 100 << "." << value % 100 << std::endl;
+        }
+        if ( (value > -100) && (value < -10))
+        {
+            std::cout << "-" << value / 100 << "." <<(-1) * (value % 100) << std::endl;
+        }
+        if ((value > -10) && (value < 0))
+        {
+            std::cout << "-0.0" << (-1) * (value % 100) << std::endl;
+        }
+        if ((value < 10) && (value > 0))
+        {
+            std::cout << "0.0" << value % 100 << std::endl;
+        }
+        if ((value >= 0) && (value < 10) && (value % 10 == 0))
+        {
+            std::cout << value / 100 << "." << value % 100 << "0" << std::endl;
+        }
+        if (value < -100)
+        {
+            std::cout << value / 100 << "." << (-1) * (value % 100) << std::endl;
+        }
+    }
+
+    money_value add (money_value &second)
+    {
+        money_value result(value / 100, value % 100);
+        result.value += second.value;
+        return result;
+    }
+
+    money_value subtract (money_value & second)
+    {
+        money_value result(value / 100, value % 100);
+        result.value -= second.value;
+        return result;
+    }
+
+    money_value multiply (int second)
+    {
+        money_value result(value / 100, value % 100);
+        result.value *= second;
+        return result;
+    }
+
+};
