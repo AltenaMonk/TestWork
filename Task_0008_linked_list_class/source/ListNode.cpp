3063558d61c4f48aa8ede1dd066e3b05b0b278e7
testr00t <mapr00tmap@gmail.com>
2015-09-28 22:26:24 +0700

    28.09.2015

--- Task_0008_linked_list_class/source/ListNode.cpp
+++ Task_0008_linked_list_class/source/ListNode.cpp
@@ -3,23 +3,28 @@
 ListNode::ListNode()
 {
 }
+
 void ListNode::SetNext(ListNode * next)
 {
     m_next = next;
 }
+
 ListNode * ListNode::GetNext()
 {
     return m_next;
 }
+
 ListNode const * ListNode::GetNext() const
 {
     return m_next;
 }
-int ListNode::GetData()
-{
-    return m_data;
-}
+
 void ListNode::SetData(int data)
 {
     m_data = data;
+}
+
+int ListNode::GetData()
+{
+    return m_data;
 }
--- Task_0008_linked_list_class/source/ListNode.h
+++ Task_0008_linked_list_class/source/ListNode.h
@@ -8,8 +8,8 @@ public:
     void SetNext(ListNode * next);
     ListNode * GetNext();
     ListNode const * GetNext() const;
-    int GetData() const;
     void SetData(int data);
+    int GetData() const;
 private:
     int m_data;
     ListNode * m_next;
