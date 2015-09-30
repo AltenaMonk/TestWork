8b912ab969f78e5e0c13af0bf0ed8e6a41c79d59
testr00t <mapr00tmap@gmail.com>
2015-09-30 20:17:49 +0700

    List 30.09.2015

--- Task_0008_linked_list_class/source/List.cpp
+++ Task_0008_linked_list_class/source/List.cpp
@@ -1,7 +1,36 @@
 #include "List.h"
+#include <cstddef>
+#include <utility>
 
+/// Constructors
 List::List()
+    :m_head(0)
 {
-
 }
 
+List::List(List const &from)
+    :m_head(from.m_head)
+{
+}
+
+/// Operator =
+List & List::operator=(List const & from)
+{
+    m_head = from.m_head;
+    return *this;
+}
+
+/// Function Swap
+void List::Swap(List &other)
+{
+    std::swap(m_head, other.m_head);
+}
+
+/// Destructor
+List::~List()
+{
+    if (m_head == NULL)
+    {
+        delete m_head;
+    }
+}
